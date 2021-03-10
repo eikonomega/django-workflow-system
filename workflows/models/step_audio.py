7288f8357e518dddd3744e47c42fac857648feaf
Michael <bigfatpanda@nd.edu>
2021-03-10 18:54:49 +0000

    Cleanup

--- workflows/models/step_audio.py
+++ workflows/models/step_audio.py
@@ -9,16 +9,6 @@ from workflows.models.step import WorkflowStep
 from workflows.utils import workflow_step_media_folder
 
 
-def workflow_step_media_folder(instance, filename):
-    """Return the location of a stored media file."""
-    return "workflows/workflows/{}/steps/{}/{}.{}".format(
-        instance.workflow_step.workflow.id,
-        instance.workflow_step_id,
-        instance.ui_identifier,
-        filename.rpartition(".")[2],
-    )
-
-
 class WorkflowStepAudio(CreatedModifiedAbstractModel):
     """
     Audio objects assigned to a WorkflowStep.
