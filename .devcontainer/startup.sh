dcbad85ab72704c0a93c87ffaea01ccb36c73e8a
Michael <bigfatpanda@nd.edu>
2021-03-17 16:53:59 +0000

    Add collectstatic to `startup.sh`

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,5 +1,6 @@
 echo "Starting Python Dev Server"
 cd /workspaces/django-workflow-system 
+python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
