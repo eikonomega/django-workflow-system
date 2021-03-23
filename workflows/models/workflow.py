6c7ba4115cc4da9b634c0f7535706e57e2bac199
Brett Fox <brettmfox33@gmail.com>
2021-03-23 09:48:32 -0600

    Develop merge

--- workflows/models/workflow.py
+++ workflows/models/workflow.py
@@ -1,3 +1,64 @@
+"""Django model definition."""
+import uuid
+
+from django.contrib.auth.models import User
+from django.db import models
+
+from workflows.models.abstract_models import CreatedModifiedAbstractModel
+from workflows.models.author import WorkflowAuthor
+from workflows.utils.validators import validate_code
+
+
+class Workflow(CreatedModifiedAbstractModel):
+    """
+    This is the primary model for this feature.
+    """
+
+    IMAGE_SIZE = (600, 375)
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        help_text="The UUID of the Workflow"
+    )
+    code = models.CharField(
+        max_length=200,
+        validators=[validate_code],
+        help_text="An internal code for database level operations"
+    )
+    name = models.CharField(max_length=200, help_text="Human friendly name")
+    version = models.PositiveIntegerField(
+        default=1,
+        help_text="The version of a Workflow. Used to accommodate the "
+                  "evolution of a Workflow over time"
+    )
+    author = models.ForeignKey(
+        WorkflowAuthor,
+        on_delete=models.PROTECT,
+        help_text="The author of the Workflow"
+    )
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        limit_choices_to={"is_staff": True},
+        help_text="Administrative user who created the Workflow in the database"
+    )
+    on_completion = models.CharField(max_length=200, null=True, blank=True)
+
+    class Meta:
+        db_table = "workflow_system_workflow"
+        unique_together = ["version", "code"]
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+
+"""Django model definition."""
 import uuid
 
 from django.contrib.auth.models import User
@@ -43,15 +104,17 @@ class Workflow(CreatedModifiedAbstractModel):
         limit_choices_to={'is_staff': True},
         help_text="Administrative user who created the Workflow in the database"
     )
-    on_completion = models.CharField(max_length=200, null=True, blank=True)
+    User, on_delete=models.PROTECT, limit_choices_to={"is_staff": True}
+)
+on_completion = models.CharField(max_length=200, null=True, blank=True)
 
-    class Meta:
-        db_table = 'workflow_system_workflow'
-        unique_together = ['version', 'code']
-        ordering = ['name']
+class Meta:
+    db_table = "workflow_system_workflow"
+    unique_together = ["version", "code"]
+    ordering = ["name"]
 
-    def __str__(self):
-        return self.name
+def __str__(self):
+    return self.name
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+def save(self, *args, **kwargs):
+    super().save(*args, **kwargs)
