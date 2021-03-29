be8718eb4817acc7fb1eaf70f0e6b90a4fa1e90c
Michael <bigfatpanda@nd.edu>
2021-03-29 17:58:30 +0000

    Add little docstrings.

--- workflows/api/serializers/user/workflows/engagement_detail.py
+++ workflows/api/serializers/user/workflows/engagement_detail.py
@@ -1,7 +1,8 @@
-import jsonschema
+"""DRF Serialzier Definition."""
 from django.urls import reverse
-
+import jsonschema
 from rest_framework import serializers
+
 
 from .....models import (
     WorkflowCollectionEngagementDetail,
@@ -29,25 +30,25 @@ class WorkflowCollectionEngagementDetailSummarySerializer(serializers.ModelSeria
     class Meta:
         model = WorkflowCollectionEngagementDetail
         fields = [
-            'detail',
-            'workflow_collection_engagement',
-            'step',
-            'user_response',
-            'started',
-            'finished'
+            "detail",
+            "workflow_collection_engagement",
+            "step",
+            "user_response",
+            "started",
+            "finished",
         ]
-        extra_kwargs = {'workflow_collection_engagement': {'write_only': True}}
+        extra_kwargs = {"workflow_collection_engagement": {"write_only": True}}
 
     def get_detail(self, instance):
         reversed_url = reverse(
-            viewname='user-workflow-collection-engagement-detail',
+            viewname="user-workflow-collection-engagement-detail",
             kwargs={
-                'engagement_id': instance.workflow_collection_engagement.id,
-                'id': instance.id
-            }
+                "engagement_id": instance.workflow_collection_engagement.id,
+                "id": instance.id,
+            },
         )
 
-        return self.context['request'].build_absolute_uri(reversed_url)
+        return self.context["request"].build_absolute_uri(reversed_url)
 
     def validate(self, data):
         """
@@ -88,43 +89,67 @@ class WorkflowCollectionEngagementDetailSummarySerializer(serializers.ModelSeria
                 return getattr(self.instance, attr_name)
             return None
 
-        step = getattr_patched('step')
-        finished = getattr_patched('finished')
-        user_response = getattr_patched('user_response')
-        has_required_inputs = bool(WorkflowStepInput.objects.filter(workflow_step=step, required=True))
-        workflow_collection_engagement = getattr_patched('workflow_collection_engagement')
-        workflow_collection: WorkflowCollection = workflow_collection_engagement.workflow_collection
+        step = getattr_patched("step")
+        finished = getattr_patched("finished")
+        user_response = getattr_patched("user_response")
+        has_required_inputs = bool(
+            WorkflowStepInput.objects.filter(workflow_step=step, required=True)
+        )
+        workflow_collection_engagement = getattr_patched(
+            "workflow_collection_engagement"
+        )
+        workflow_collection: WorkflowCollection = (
+            workflow_collection_engagement.workflow_collection
+        )
 
         state = workflow_collection_engagement.state
 
-        if not workflow_collection.workflowcollectionmember_set.filter(workflow__workflowstep=step):
-            raise serializers.ValidationError('Step must belong to a workflow in the collection')
+        if not workflow_collection.workflowcollectionmember_set.filter(
+            workflow__workflowstep=step
+        ):
+            raise serializers.ValidationError(
+                "Step must belong to a workflow in the collection"
+            )
 
         # Usually, the step must be either state['next_step_id'] or state['prev_step_id']
         # However, in an ACTIVITY workflow collection, if the user has not submitted any
         # engagement details, the step can be any first step of a workflow
-        if workflow_collection.category == 'ACTIVITY':
-            existing_engagement_details = workflow_collection_engagement.workflowcollectionengagementdetail_set.all()
+        if workflow_collection.category == "ACTIVITY":
+            existing_engagement_details = (
+                workflow_collection_engagement.workflowcollectionengagementdetail_set.all()
+            )
             if not existing_engagement_details:
-                if WorkflowStep.objects.filter(workflow=step.workflow, order__lt=step.order):
-                    raise serializers.ValidationError("Posted step must be the first step in a workflow")
+                if WorkflowStep.objects.filter(
+                    workflow=step.workflow, order__lt=step.order
+                ):
+                    raise serializers.ValidationError(
+                        "Posted step must be the first step in a workflow"
+                    )
             else:
-                if step.id not in (state['next_step_id'], state['prev_step_id']):
-                    raise serializers.ValidationError("Posted step must be next step or previous step.")
-        elif workflow_collection.category == 'SURVEY':
-            if step.id not in (state['next_step_id'], state['prev_step_id']):
-                raise serializers.ValidationError("Posted step must be next step or previous step.")
+                if step.id not in (state["next_step_id"], state["prev_step_id"]):
+                    raise serializers.ValidationError(
+                        "Posted step must be next step or previous step."
+                    )
+        elif workflow_collection.category == "SURVEY":
+            if step.id not in (state["next_step_id"], state["prev_step_id"]):
+                raise serializers.ValidationError(
+                    "Posted step must be next step or previous step."
+                )
 
         if not finished:
             return data
 
         if has_required_inputs:  # then having no answers is bad
             if not user_response:
-                raise serializers.ValidationError("workflow_step has required step_input(s) but "
-                                                  "workflow_collection_engagement has no user_response")
+                raise serializers.ValidationError(
+                    "workflow_step has required step_input(s) but "
+                    "workflow_collection_engagement has no user_response"
+                )
             if "questions" not in user_response:
-                raise serializers.ValidationError("workflow_step has required step_input(s) but "
-                                                  "user_response does not contain key \"questions\"")
+                raise serializers.ValidationError(
+                    "workflow_step has required step_input(s) but "
+                    'user_response does not contain key "questions"'
+                )
         else:  # having no answers is ok
             if not user_response:
                 return data
@@ -139,22 +164,34 @@ class WorkflowCollectionEngagementDetailSummarySerializer(serializers.ModelSeria
                 step_input_UI_identifier = answer["stepInputUIIdentifier"]
                 response = answer["response"]
             except KeyError as e:
-                raise serializers.ValidationError('Missing key in questions entry {}'.format(e.args[0]))
-            if not WorkflowStepInput.objects.filter(id=step_input_id, ui_identifier=step_input_UI_identifier):
-                raise serializers.ValidationError('No step with given stepInputID and stepInputUIIdentifier exists')
+                raise serializers.ValidationError(
+                    "Missing key in questions entry {}".format(e.args[0])
+                )
+            if not WorkflowStepInput.objects.filter(
+                id=step_input_id, ui_identifier=step_input_UI_identifier
+            ):
+                raise serializers.ValidationError(
+                    "No step with given stepInputID and stepInputUIIdentifier exists"
+                )
             answer_dict[step_input_id] = response
-
 
         for step_input in WorkflowStepInput.objects.filter(workflow_step=step):
             step_input_id = str(step_input.id)
             if step_input_id not in answer_dict:  # if user did not give an answer...
                 if step_input.required:  # that's a problem if it was required
-                    raise serializers.ValidationError('Missing response to step_input id {}'.format(step_input_id))
+                    raise serializers.ValidationError(
+                        "Missing response to step_input id {}".format(step_input_id)
+                    )
             else:  # user gave an answer, and we should validate it
                 response = answer_dict[step_input_id]
                 try:
-                    jsonschema.validate(instance=response, schema=step_input.response_schema.schema)
+                    jsonschema.validate(
+                        instance=response, schema=step_input.response_schema.schema
+                    )
                 except jsonschema.ValidationError as e:
                     raise serializers.ValidationError(
-                        'Response did not match workflow step input response schema {}'.format(e.message))
+                        "Response did not match workflow step input response schema {}".format(
+                            e.message
+                        )
+                    )
         return data
