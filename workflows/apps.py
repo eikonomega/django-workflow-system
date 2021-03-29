77663ed9ff40340c056a8e0c3251ff2886bb2944
Brett Fox <brettmfox33@gmail.com>
2021-03-29 09:39:59 -0600

    Added AUTHENTICATION_BACKENDS check

--- workflows/apps.py
+++ workflows/apps.py
@@ -1,10 +1,12 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class WorkflowsConfig(AppConfig):
     name = 'workflows'
 
     def ready(self):
-        print("Warning: DRF Authentication settings have not been set. "
-              "The application will default to session auth and basic auth. "
-              "See https://www.django-rest-framework.org/api-guide/authentication/#setting-the-authentication-scheme")
+        if settings.AUTHENTICATION_BACKENDS == ['django.contrib.auth.backends.ModelBackend']:
+            print("Warning: DRF Authentication settings have not been set. "
+                  "The application will default to session auth and basic auth. "
+                  "For more information see https://www.django-rest-framework.org/api-guide/authentication/#setting-the-authentication-scheme")
