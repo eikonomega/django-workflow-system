61627d535b8b778c3068dc666f7bdb0b71d18bd7
Brett Fox <bfox6@nd.edu>
2021-04-15 10:14:52 -0600

    Fixed config files for pypi release

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+def read(f):
+    return open(f, encoding='utf-8').read()
+
 setup(
     name="django-workflow-system",
     version="0.1",
     description="A Django app to conduct surveys/activities.",
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     author="Center for Research Computing",
     author_email="prometheus-team@nd.edu",
     license="MIT",
