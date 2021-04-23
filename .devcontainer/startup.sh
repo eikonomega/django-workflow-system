c631697e504954ccd71812b54a89ec0778b53f23
Justin Branco <jbranco@nd.edu>
2021-04-23 16:59:47 +0000

    Update startup script for static issues

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,6 +1,6 @@
 echo "Starting Python Dev Server"
 cd /workspaces/django-workflow-system 
-python manage.py collectstatic
+python manage.py collectstatic  --noinput
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
