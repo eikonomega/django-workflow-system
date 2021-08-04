a29ecf2eab09c25681b9e39c6bb4cd878267e49b
Michael <bigfatpanda@nd.edu>
2021-08-04 18:51:00 +0000

    Fix bug in `multiple_choice_question.py`
    Not all possible combinations of selected options were being generated.

--- django_workflow_system/utils/response_schema_handlers/multiple_choice_question.py
+++ django_workflow_system/utils/response_schema_handlers/multiple_choice_question.py
@@ -1,6 +1,7 @@
-"""Response Schema Generator Module for a Multiple Choice Question"""
+"""Response Schema Generator Module for a Multiple Choice Question."""
 import copy
 import itertools
+
 from django_workflow_system.utils import RESPONSE_SCHEMA
 
 
@@ -17,88 +18,69 @@ def get_response_schema(workflow_step_user_input):
     """
     response_schema = copy.deepcopy(RESPONSE_SCHEMA)
 
-    # Now we need to check some things.....
+    # Scenario 1 They need to respond with the correct answer.
     if (
         workflow_step_user_input.specification["meta"]["inputRequired"]
         and workflow_step_user_input.specification["meta"]["correctInputRequired"]
     ):
-        # They need to respond with the correct answer.
-        response_schema["properties"]["userInput"][
-            "type"
-        ] = workflow_step_user_input.type.json_schema["properties"]["correctInput"][
-            "type"
-        ]
+        # Answer must be an array.
+        response_schema["properties"]["userInput"]["type"] = "array"
 
         response_schema["properties"]["userInput"][
             "enum"
-        ] = fetch_all_combinations_unordered(
+        ] = correct_answer_combinations(
             workflow_step_user_input.specification["correctInput"]
         )
 
+    # Scenario 2: They need to answer, but not necessarily correctly.
     elif (
         workflow_step_user_input.specification["meta"]["inputRequired"]
         and not workflow_step_user_input.specification["meta"]["correctInputRequired"]
     ):
-        # They don't need to respond with the correct answer.
-        response_schema["properties"]["userInput"][
-            "type"
-        ] = workflow_step_user_input.type.json_schema["properties"]["correctInput"][
-            "type"
-        ]
-        response_schema["properties"]["userInput"]["enum"] = fetch_all_combinations(
-            workflow_step_user_input
+        # Answer must be an array.
+        response_schema["properties"]["userInput"]["type"] = "array"
+
+        # Get all possible combinations of input options.
+        response_schema["properties"]["userInput"]["enum"] = all_possible_combinations(
+            workflow_step_user_input.specification["inputOptions"]
         )
 
+    # Scenario 3: Answer is not required and correct is not required, so null should be an option in potential responses
     else:
-        # Answer is not required and correct is not required, so null should be an option in potential responses
-        response_schema["properties"]["userInput"]["anyOf"] = [
-            {
-                "type": workflow_step_user_input.type.json_schema["properties"][
-                    "correctInput"
-                ]["type"]
-            },
-            {"type": "null"},
-        ]
-        new_enum = fetch_all_combinations(workflow_step_user_input)
+        response_schema["properties"]["userInput"]["type"] = ["array", "null"]
+
+        new_enum = all_possible_combinations(
+            workflow_step_user_input.specification["inputOptions"]
+        )
         new_enum.append(None)
         response_schema["properties"]["userInput"]["enum"] = new_enum
 
     return response_schema
 
 
-def fetch_all_combinations(workflow_step_user_input):
+def all_possible_combinations(input_options):
     """
-    Fetch all possible combinations of a multi choice question.
+    Determine all possible combinations for a given set of input options.
 
     Args:
-        workflow_step_user_input (WorkflowStepUserInput): The WorkflowStepUserInput Object.
-
-    Returns:
-        list: All possible combinations of available answers.
+        input_options: A list of options.
     """
-    # We need to get the range of input options, loop through and create a monster
     all_combos = []
-    for number in range(
-        1, len(workflow_step_user_input.specification["inputOptions"]) + 1
-    ):
-        all_combos += [
-            list(combo)
-            for combo in itertools.combinations(
-                workflow_step_user_input.specification["inputOptions"], number
-            )
-        ]
+    for number_of_selected_answers in range(len(input_options)):
+        for potential_combination in itertools.permutations(
+            input_options, number_of_selected_answers + 1
+        ):
+            all_combos.append(list(potential_combination))
+
     return all_combos
 
 
-def fetch_all_combinations_unordered(correct_answer):
+def correct_answer_combinations(correct_answer):
     """
-    Fetch all possible combinations of a multi choice question, where order does not matter.
+    Determine all possible orderings for the "correct answers" to a multi choice question.
 
     Args:
-        workflow_step_user_input (WorkflowStepUserInput): The Workflow correct input.
-
-    Returns:
-        list: All possible combinations of available answers.
+        correct_answer: An array who elements comprised the correct answer to the question.
     """
 
     combos = itertools.permutations(correct_answer)
